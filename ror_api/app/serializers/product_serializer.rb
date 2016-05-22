a9ee4a8c66fd2b4cfe8c236a1f381f09c66f505a
Kleber Pinel <kleber.silva@toptal.com>
2016-05-22 12:10:53 -0700

    adding default icon url

--- ror_api/app/serializers/product_serializer.rb
+++ ror_api/app/serializers/product_serializer.rb
@@ -8,4 +8,16 @@ class ProductSerializer < ActiveModel::Serializer
   def price
     object.price
   end
+
+  def image_url
+    turtles_icon = [
+      "http://icons.iconarchive.com/icons/icons8/windows-8/512/Cinema-Ninja-Turtle-icon.png",
+      "http://www.thinkgeek.com/images/blog/2012pumpkin-20-icon.jpg"
+    ]
+    if object.image_url == ""
+      turtles_icon[rand(0..turtles_icon.size-1)]
+    else
+      object.image_url
+    end
+  end
 end
