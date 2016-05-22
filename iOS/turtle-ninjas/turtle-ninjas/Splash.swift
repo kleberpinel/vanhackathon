024aafc2a727bc65075171c5df2a3fcd2b600c50
Vitor Oliveira <vbrazo@gmail.com>
2016-05-22 15:31:31 -0300

    Adds a function that checks whether the connected is on or off - loading view

--- iOS/turtle-ninjas/turtle-ninjas/Splash.swift
+++ iOS/turtle-ninjas/turtle-ninjas/Splash.swift
@@ -11,16 +11,46 @@ import UIKit
 // Splash - load models
 class Splash: UIViewController {
 
+    @IBOutlet weak var labelRefreshIcon: UIButton!
+    @IBOutlet weak var labelDescription: UILabel!
+    @IBOutlet weak var labelInternetConnection: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let stores = mStores()
-        stores.populate_data() { (response) in
-            dispatch_async(dispatch_get_main_queue()) {
-                self.performSegueWithIdentifier("transSplashToMap", sender: nil)
+        self.load_data()
+    }
+    
+    func load_data(){
+        if Connection.isON() {
+            notConnected(false)
+            let stores = mStores()
+            stores.populate_data() { (response) in
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.performSegueWithIdentifier("transSplashToMap", sender: nil)
+                }
             }
+        } else {
+            self.labelDescription.hidden = true
+            self.labelRefreshIcon.hidden = false
+            self.labelInternetConnection.hidden = false
         }
-        
+    }
+    
+    @IBAction func refreshIconClick(sender: AnyObject) {
+        load_data()
+    }
+    
+    // hide and show label if the internet is on or off
+    func notConnected(status: Bool){
+        if status == false {
+            self.labelDescription.hidden = false
+            self.labelRefreshIcon.hidden = true
+            self.labelInternetConnection.hidden = true
+        } else {
+            self.labelDescription.hidden = true
+            self.labelRefreshIcon.hidden = false
+            self.labelInternetConnection.hidden = false
+        }
     }
     
 }
