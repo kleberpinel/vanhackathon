db70e5480db55772c95e961576f9fcc4e6781b1e
Vitor Oliveira <vbrazo@gmail.com>
2016-05-22 01:50:38 -0300

    Adds splash screen to load models before displaying the map

---
+++ iOS/turtle-ninjas/turtle-ninjas/Schema.swift
@@ -0,0 +1,9 @@
+//
+//  Schema.swift
+//  turtle-ninjas
+//
+//  Created by Vitor Oliveira on 5/22/16.
+//  Copyright © 2016 Vitor Oliveira. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/turtle-ninjas/turtle-ninjas/mStores.swift
@@ -0,0 +1,9 @@
+//
+//  mStores.swift
+//  turtle-ninjas
+//
+//  Created by Vitor Oliveira on 5/22/16.
+//  Copyright © 2016 Vitor Oliveira. All rights reserved.
+//
+
+import Foundation
