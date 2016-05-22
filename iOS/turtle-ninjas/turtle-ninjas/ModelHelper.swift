e216dbe9e99e690297b29106a2468b1747d8fbb1
Vitor Oliveira <vbrazo@gmail.com>
2016-05-22 02:37:15 -0300

    Adds ModelHelper

--- iOS/turtle-ninjas/turtle-ninjas/ModelHelper.swift
+++ iOS/turtle-ninjas/turtle-ninjas/ModelHelper.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2016 Vitor Oliveira. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+struct ModelHelper {
+    
+    internal let managedContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    
+    func delete_all(entity: String){
+        
+        let fetchRequest = NSFetchRequest(entityName: entity)
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try managedContext.executeFetchRequest(fetchRequest)
+            for managedObject in results {
+                let managedObjectData:NSManagedObject = managedObject as! NSManagedObject
+                managedContext.deleteObject(managedObjectData)
+            }
+        } catch let error as NSError {
+            print("Detele all - error : \(error) \(error.userInfo)")
+        }
+        
+    }
+    
+}
